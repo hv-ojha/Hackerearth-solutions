5cf3670b9c3fd6fce19399cde4ecc35da279922f
Harshvardhan Ojha <42267022+hv-ojha@users.noreply.github.com>
2019-05-16 21:28:05 +0530

    Create prime-number.java

---
+++ prime-number.java
@@ -0,0 +1,54 @@
+/* IMPORTANT: Multiple classes and nested static classes are supported */
+
+/*
+ * uncomment this if you want to read input.
+//imports for BufferedReader
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+*/
+//import for Scanner and other utility classes
+import java.util.*;
+
+// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        /* Sample code to perform I/O:
+         * Use either of these methods for input
+
+        //BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String name = br.readLine();                // Reading input from STDIN
+        System.out.println("Hi, " + name + ".");    // Writing output to STDOUT
+
+        //Scanner
+        Scanner s = new Scanner(System.in);
+        String name = s.nextLine();                 // Reading input from STDIN
+        System.out.println("Hi, " + name + ".");    // Writing output to STDOUT
+
+        */
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        try
+        {
+            for(int i=1;i <= n;i++) {
+                int j=i-1;
+                while(j>1) {
+                    if(i%j==0) {
+                        break;
+                    }
+                    j--;
+                }
+                if(j==1)
+                    System.out.print(i + " ");
+            }
+        }
+        catch(Exception ex)
+        {
+            System.out.println(ex);
+        }
+
+        // Write your code here
+
+    }
+}
