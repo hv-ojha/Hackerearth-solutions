369247a469ad50b9e1ae3abe62760ac237122670
Harshvardhan Ojha <42267022+hv-ojha@users.noreply.github.com>
2019-05-20 14:45:32 +0530

    problem statement added in prime numbers

--- Basics of Input Output/prime-number.java
+++ Basics of Input Output/prime-number.java
@@ -1,3 +1,20 @@
+/* 
+You are given an integer N. You need to print the series of all prime numbers till N.
+
+Input Format
+
+The first and only line of the input contains a single integer N denoting the number till where you need to find the series of prime number.
+
+Output Format
+
+Print the desired output in single line separated by spaces.
+
+Constraints
+
+1<=N<=1000
+*/
+
+
 /* IMPORTANT: Multiple classes and nested static classes are supported */
 
 /*
