78431fd327b19f1641c4ccc7c7db22d9ade5b62e
Harshvardhan Ojha <42267022+hv-ojha@users.noreply.github.com>
2019-05-20 14:43:36 +0530

    Update seating-arrangement.java

--- Basics of Input Output/seating-arrangement.java
+++ Basics of Input Output/seating-arrangement.java
@@ -1,4 +1,4 @@
-/* <h1>PROBLEM STATEMENT :- </h1>
+/* PROBLEM STATEMENT :-
 Akash and Vishal are quite fond of travelling. They mostly travel by railways.
 They were travelling in a train one day and they got interested in the seating arrangement of their compartment. 
 The compartment looked something like
